c9aa1e3dd0a553308ea40f19c913b893841a686c
ghills <ghills@gmail.com>
2009-08-31 11:19:56 -0700

    refactored to use generic views. Finished through part 3 of the tutorial

--- polls/views.py
+++ polls/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import render_to_response, get_object_or_404
 from mysite.polls.models import Poll
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
-    
-def detail(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': p})
     
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
@@ -17,7 +9,7 @@ def vote(request, poll_id):
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the poll voting form.
-        return render_to_response('polls/detail.html', {
+        return render_to_response('polls/poll_detail.html', {
                                   'poll': p,
                                   'error_message': "You didn't select a choice.",
         })
@@ -27,8 +19,4 @@ def vote(request, poll_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('mysite.polls.views.results', args=(p.id,)))
-        
-def results(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/results.html', {'poll': p})
+        return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
